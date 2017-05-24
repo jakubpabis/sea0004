64d94367a385a15de142b886642dc93ecd7e58c4
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-05-24 22:20:33 +0100

    add Siema carousel, add references section to home page. Home page desktop view completed

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -29,3 +29,42 @@ function hideSearch()
 	document.getElementById('search-field-general').classList.remove('active');
 	document.getElementsByClassName('search-btn')[0].classList.remove('active');
 }
+
+function siemaAutoplay($time, $siema, $carousel) {
+	var timer = setInterval(function() {
+		$siema.next();
+	}, $time);
+	$carousel.addEventListener('mouseenter', function() {
+		clearInterval(timer);
+	});
+	$carousel.addEventListener('mouseleave', function() {
+		timer = setInterval(function() {
+			$siema.next();
+		}, $time);
+	});
+}
+
+function loadCarousel() {
+	var siema = document.getElementById('siema-carousel');
+	if(siema) {
+		const mySiema = new Siema({
+			selector: '#siema-carousel',
+			duration: 500,
+			easing: 'ease',
+			perPage: 1,
+			startIndex: 0,
+			draggable: true,
+			threshold: 20,
+			loop: true
+		});
+		document.getElementById('siema-prev').addEventListener('click', function() {
+			mySiema.prev()
+		});
+		document.getElementById('siema-next').addEventListener('click', function() {
+			mySiema.next()
+		});
+		siemaAutoplay(5000, mySiema, siema);
+	}
+}
+
+loadCarousel();
