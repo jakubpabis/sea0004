3ae0d32a457ce1a827d6b7aae4618c715b5b71c4
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-06-04 21:21:36 +0100

    responsive navigation almost done

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -15,10 +15,15 @@ if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
 else window.addEventListener('load', loadDeferredStyles);
 //-------------------- /Load some deferred styles --------------------//
 
+function hasClass(el, cls) {
+	return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
+}
+
 function openSearch($el) 
 {
 	$el.classList.add('active');
 	document.getElementById('search-field-general').classList.add('active');
+	document.getElementById('header-social-links').classList.add('active');
 	setTimeout(function() {
 		document.getElementById('search-field-general-input').focus();
 	}, 500);
@@ -27,7 +32,47 @@ function openSearch($el)
 function hideSearch() 
 {
 	document.getElementById('search-field-general').classList.remove('active');
+	document.getElementById('header-social-links').classList.remove('active');
 	document.getElementsByClassName('search-btn')[0].classList.remove('active');
+}
+
+function showLang($el, event) {
+	if(window.innerWidth <= 760 && !hasClass($el, 'active')) {
+		event.preventDefault();
+		$el.classList.add('active');
+	} else if(window.innerWidth <= 760 && hasClass($el, 'active')) {
+		event.preventDefault();
+		$el.classList.remove('active');
+	}
+}
+
+function showMenu($el) {
+	if(!hasClass($el, 'active')) {
+		$el.classList.add('active');
+		document.getElementsByTagName('body')[0].classList.add('menu-active');
+		document.getElementsByTagName('html')[0].classList.add('menu-active');
+		document.getElementById('wrapper').classList.add('menu-active');
+		setTimeout(function() {
+			document.querySelectorAll('*:not(.menu)').forEach(function(elem) {
+				elem.addEventListener('click', hideMenu);
+			});
+		}, 300);
+	} else {
+		$el.classList.remove('active');
+		document.getElementsByTagName('body')[0].classList.remove('menu-active');
+		document.getElementsByTagName('html')[0].classList.remove('menu-active');
+		document.getElementById('wrapper').classList.remove('menu-active');
+	}
+}
+
+function hideMenu() {
+	document.getElementById('menu-btn').classList.remove('active');
+	document.getElementsByTagName('body')[0].classList.remove('menu-active');
+	document.getElementsByTagName('html')[0].classList.remove('menu-active');
+	document.getElementById('wrapper').classList.remove('menu-active');
+	document.querySelectorAll('*:not(.menu)').forEach(function(elem) {
+		elem.removeEventListener('click', hideMenu);
+	});
 }
 
 function siemaAutoplay($time, $siema, $carousel) {
