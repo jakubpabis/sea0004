0f3f44aa7e50a8c1b3191f2e42625a9e5c2afeba
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-06-28 23:05:53 +0100

    start contact page

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -165,7 +165,7 @@ function hideCVForm()
 	}, 500);
 }
 
-function initMap()
+function initJobMap()
 {
 	var job_map = document.getElementById('job_map');
 	var map = new google.maps.Map(job_map, {
@@ -180,6 +180,106 @@ function initMap()
 	var marker = new google.maps.Marker({
 		map: map,
 		position: new google.maps.LatLng(52.3702157,4.8951679)
+	});
+	map.set('styles', 
+		[
+			{
+				"featureType": "administrative",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#444444"
+					}
+				]
+			},
+			{
+				"featureType": "landscape",
+				"elementType": "all",
+				"stylers": [
+					{
+						"color": "#f2f2f2"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "all",
+				"stylers": [
+					{
+						"saturation": -100
+					},
+					{
+						"lightness": 45
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "simplified"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "labels.icon",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "transit",
+				"elementType": "all",
+				"stylers": [
+					{
+						"visibility": "off"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "all",
+				"stylers": [
+					{
+						"color": "#7f8ec1"
+					},
+					{
+						"visibility": "on"
+					}
+				]
+			}
+		]
+	);
+}
+
+function initContactMap()
+{
+	var contact_map = document.getElementById('contact_map');
+	var map = new google.maps.Map(contact_map, {
+		center: {lat: 52.3214278, lng: 4.876879},
+		zoom: 14,
+		scrollwheel: false,
+		draggable: true,
+		mapTypeControl: false,
+		scaleControl: true,
+		streetViewControl: true
+	});
+	var marker = new google.maps.Marker({
+		map: map,
+		position: new google.maps.LatLng(52.3214278,4.876879)
 	});
 	map.set('styles', 
 		[
