33641829ddb80b9584dd2041de4f782c49d91e8a
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2017-07-24 23:06:20 +0100

    add testimonials plugin

---
+++ build/plugins/searchit/testimonials/updates/builder_table_create_searchit_testimonials_testimonials.php
@@ -0,0 +1,25 @@
+<?php namespace Searchit\Testimonials\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateSearchitTestimonialsTestimonials extends Migration
+{
+    public function up()
+    {
+        Schema::create('searchit_testimonials_testimonials', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->unsigned();
+            $table->text('summary');
+            $table->string('who');
+            $table->string('type');
+            $table->boolean('featured')->default(0);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('searchit_testimonials_testimonials');
+    }
+}
