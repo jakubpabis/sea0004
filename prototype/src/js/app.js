d3adc6815c7193a3826398e69ce9818debaabd83
Jakub Pabis <jakub@honestideas.co.uk>
2017-09-28 10:24:01 +0100

    change bit with Gfonts and icons

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 //-------------------- Load some deferred styles --------------------//
-var loadDeferredStyles = function() 
-{
-	var addStylesNode = document.getElementById('deferred-styles');
-	var replacement = document.createElement('div');
-	replacement.innerHTML = addStylesNode.textContent;
-	document.body.appendChild(replacement);
-	addStylesNode.parentElement.removeChild(addStylesNode);
-}
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-    webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
-else window.addEventListener('load', loadDeferredStyles);
+// var loadDeferredStyles = function() 
+// {
+// 	var addStylesNode = document.getElementById('deferred-styles');
+// 	var replacement = document.createElement('div');
+// 	replacement.innerHTML = addStylesNode.textContent;
+// 	document.body.appendChild(replacement);
+// 	addStylesNode.parentElement.removeChild(addStylesNode);
+// }
+// var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+//     webkitRequestAnimationFrame || msRequestAnimationFrame;
+// if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+// else window.addEventListener('load', loadDeferredStyles);
 //-------------------- /Load some deferred styles --------------------//
 
 // Smartsupp Live Chat script
