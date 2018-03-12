76710b9650eb553a19846f1a5c8e57bdb21d7170
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2018-03-12 21:03:37 +0000

    again

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -11,76 +11,6 @@ window.smartsupp||(function(d) {
 	c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
 })(document);
 
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '382574281913074',
-		xfbml      : true,
-		version    : 'v2.5'
-	});
-};
-
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function myFacebookLogin() {
-	FB.init({
-		appId      : '382574281913074',
-		xfbml      : true,
-		version    : 'v2.5'
-	});
-	FB.login(function(response){
-		if (response.status === 'connected') {
-			console.log('Logged in.');
-			FB.api('/me', {fields: 'name, email, location, gender'}, function(response1){
-				$formCont.find('input[name="applicant-name"]').val(response1['name']);
-				$formCont.find('input[name="applicant-email"]').val(response1['email']);
-				$formCont.find('textarea[name="applicant-address"]').val(response1['location']);
-				$formCont.find('input[name="applicant-gender"]').val(response1['gender']);
-			});
-			FB.api('/me/picture',{"redirect": false, "height": 200, "width": 200, "type": "normal"}, function(response2){
-				$formCont.find('input[name="applicant-photo"]').val(response2['data']['url']);
-			});
-		} else {
-			FB.login();
-		}
-	}, {scope: 'publish_actions'} );
-}
-
-function liAuth(){
-	IN.User.authorize(function(){
-		onLinkedInLoad();
-	});
-}
-
-function OnLinkedInFrameworkLoad() {
-	IN.Event.on(IN, "auth", onLinkedInLoad);
-}
-// Setup an event listener to make an API call once auth is complete
-function onLinkedInLoad() {
-	IN.API.Profile("me").result(getProfileData);
-}
-// Handle the successful return from the API call
-function onSuccess(data) {
-	console.log(data);
-	$formCont.find('input[name="applicant-name"]').val(data['firstName']+' '+data['lastName']);
-	$formCont.find('input[name="applicant-email"]').val(data['emailAddress']);
-	$formCont.find('textarea[name="applicant-address"]').val(data['location']['name']);
-	$formCont.find('input[name="applicant-photo"]').val(data['pictureUrl']);
-}
-// Handle an error response from the API call
-function onError(error) {
-	console.log(error);
-}
-// Use the API call wrapper to request the member's basic profile data
-function getProfileData() {
-	IN.API.Raw("/people/~:(first-name,last-name,picture-url,email-address,location)").result(onSuccess);
-}
-
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -575,12 +505,3 @@ if(document.getElementById('uploadCvModal')) {
 	}, false);
 }
 
-var $formCont;
-$(document).ready(function() {
-	if($('#jobFormModal').length) {
-		$formCont = $('#uploadCvModal, #jobFormModal');
-	} else {
-		$formCont = $('#uploadCvModal');
-	}
-	console.log(document.referrer);
-});
