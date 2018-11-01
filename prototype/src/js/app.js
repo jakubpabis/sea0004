80410c73ffb1969623581286148b2002d4df735e
Jakub Pabis <pabis91@gmail.com>
2018-11-01 22:41:07 +0100

    changes for not sending twice the same form for job and cv

--- prototype/src/js/app.js
+++ prototype/src/js/app.js
@@ -423,6 +423,41 @@ function getReferrer()
 
 }
 
+function onFormSubmit()
+{
+	$(document).on('submit', 'form', function() {
+		$(this).addClass('disabled').find('input[type="submit"]').attr("disabled", true).addClass('disabled');
+		if($(this).hasClass('application-form')) {
+			setCookie('jobid_'+$(this).find('input.job-id').val(), $(this).find('input.job-id').val(), 365);
+		} else if($(this).hasClass('cv-form')) {
+			setCookie('cvform', 'sent', 365);
+		}
+	});
+}
+
+function onFormLoad()
+{
+	var $jobid = $('form.application-form').find('input.job-id').val();
+	var $cookie = getCookie('jobid_'+$jobid);
+
+	if(getCookie('cvform')) {
+		$('form.cv-form').addClass('cv-sent');
+	}
+
+	if($jobid !== 188 && $jobid == getCookie('jobid_'+$jobid)) {
+		$('form.application-form').addClass('application-sent');
+	}
+
+}
+
+function unblurApplication() {
+	$('.application-form').removeClass('application-sent');
+}
+
+function unblurCV() {
+	$('.cv-form').removeClass('cv-sent');
+}
+
 function menuScroll()
 {
 	var $nav = $('.main-navigation');
@@ -452,4 +487,6 @@ $(document).ready(function() {
 	}
 
 	getReferrer();
+	onFormSubmit();
+	onFormLoad();
 });
