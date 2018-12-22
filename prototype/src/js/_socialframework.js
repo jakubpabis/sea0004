76abde8f6c98002a19319ca3f591980cb3b46ecf
Jakub Pabis <pabis91@gmail.com>
2018-12-22 17:24:19 +0100

    some small for facebook widget loading

--- prototype/src/js/_socialframework.js
+++ prototype/src/js/_socialframework.js
@@ -1,18 +1,23 @@
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '382574281913074',
-		xfbml      : true,
-		version    : 'v3.0'
-	});
-};
+$(window).on('load', function() {
+	
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '382574281913074',
+			xfbml      : true,
+			version    : 'v3.0'
+		});
+	};
+	
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
 
-(function(d, s, id){
-	var js, fjs = d.getElementsByTagName(s)[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement(s); js.id = id;
-	js.src = "//connect.facebook.net/en_US/sdk.js";
-	fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
+});
 
 function myFacebookLogin() {
 	FB.init({
