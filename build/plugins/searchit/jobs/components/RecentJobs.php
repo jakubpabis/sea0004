9e5561e420bdcf709a5e8068fe214f8ff09be596
Jakub Pabis <pabis91@gmail.com>
2018-12-23 14:35:40 +0100

    referrer changes

--- build/plugins/searchit/jobs/components/RecentJobs.php
+++ build/plugins/searchit/jobs/components/RecentJobs.php
@@ -2,6 +2,8 @@
 
 use Cms\Classes\ComponentBase;
 use Searchit\Jobs\Models\Job;
+use Session;
+use Request;
 
 class RecentJobs extends ComponentBase
 {
@@ -19,8 +21,19 @@ class RecentJobs extends ComponentBase
     public function onRun()
     {
 
-      $this->jobs = new Job;
-      $this->page['recent'] = $this->jobs->orderBy('date', 'desc')->paginate(12);
+        $this->jobs = new Job;
+        $this->page['recent'] = $this->jobs->orderBy('date', 'desc')->paginate(12);
+
+        if(Session::has('referrer')) {
+            $this->page['theReferrer'] = Session::get('referrer');
+        } else {
+            if(preg_match('/gclid/i', Request::server('HTTP_REFERER')) or preg_match('/gclid/i', Request::server('REQUEST_URI'))) {
+                Session::put('referrer', 'AdWords');
+            } else {
+                Session::put('referrer', Request::server('HTTP_REFERER'));
+            }
+            $this->page['theReferrer'] = Session::get('referrer');
+        }
 
     }
 
