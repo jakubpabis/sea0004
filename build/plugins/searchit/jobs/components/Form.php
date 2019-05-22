af7b8c6e9c1e58532ebf607561a4d868885427be
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2019-05-22 09:35:07 +0200

    small

--- build/plugins/searchit/jobs/components/Form.php
+++ build/plugins/searchit/jobs/components/Form.php
@@ -175,163 +175,68 @@ class Form extends ComponentBase
 		} else {
 
             if(env('APP_ENV') !== 'dev') {
-
-                // if(Input::hasFile('applicant-cv')) {
-
-                //     if(Input::file('applicant-cv')->getSize() < (2097152)) { //can't be larger than 2 MB
-                //         $file = new FileSys;
-                //         $file->data = Input::file('applicant-cv');
-                //         $file->save();
-                //     } else {
-                //         throw new ValidationException('Oops!  Your file\'s size is to large.');
-                //     }
-
-                // }
-                
-                // if (!function_exists('curl_file_create')) {
-                //     function curl_file_create($filename, $mimetype = '', $postname = '') {
-                //         return "@$filename;filename="
-                //             . ($postname ?: basename($filename))
-                //             . ($mimetype ? ";type=$mimetype" : '');
-                //     }
-                // }
-
-                // //$signature = bin2hex(hash_hmac('sha1', $this->endpoint.'/'.$this->key, $this->secret, true));
-                // //$uri = "http://api.searchsoftware.nl/{$this->endpoint}?api_key={$this->key}&signature={$signature}";
-                // // Set up the url
-
-                // $application_data = array(
-                //     'name' => Input::get('applicant-name'),
-                //     'email' => Input::get('applicant-email'),
-                //     'gender' => Input::get('gender'),
-                //     'birthdate' => Input::get('dob'),
-                //     'location' => array(
-                //         'line1' => Input::get('applicant-street'),
-                //         'city' => Input::get('applicant-city'),
-                //         'zip' => Input::get('applicant-zip'), 
-                //         'country' => Input::get('applicant-country'),
-                //     ),
-                //     'phone' => Input::get('applicant-phone'),
-                //     'note' => array(
-                //         'text' => Input::get('applicant-message')
-                //     ),
-                //     'job' => array(
-                //         'id' => Input::get('job-id')
-                //     ),
-                //     'sources' => array(
-                //         array(
-                //             'parent_source_id' => Input::get('applicant-find'),
-                //             'name' => 'Applicant'
-                //         )
-                //     )
-                // );
-
-                // if($application_data['location']['country'] === NULL) {
-                //     unset($application_data['location']['country']);
-                // }
-
-                // //var_dump($application_data);
-
-                // if(Input::hasFile('applicant-cv')){
-                //     $uploaded_file = Input::file('applicant-cv')->getRealPath();
-                //     $file_ext = Input::file('applicant-cv')->getMimeType();
-                //     $file_name = Input::file('applicant-cv')->getClientOriginalName();
-                //     $file_cv = curl_file_create($uploaded_file, $file_ext, $file_name);
-                //     $data = array(
-                //         'json' => json_encode($application_data),
-                //         'cv' => $file_cv
-                //     );
-                // } else {
-                //     $data = array(
-                //         'json' => json_encode($application_data)
-                //     );
-                // }
-
-                // //initialise the curl request
-                // $request = curl_init();
-
-                // curl_setopt($request, CURLOPT_URL, $uri);
-                // curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-                // curl_setopt($request, CURLOPT_POST, 1);
-                // curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-
-                // $reply = curl_exec($request);
-                // Log::info('CURL logging for form submition: '.$reply);
-                // Log::info('Candidate Name: '.Input::get('applicant-name').'; Source: '.Input::get('applicant-find'));
-                // // close the session
-                // curl_close($request);
-
                 $this->add_to_queue();
+            }
 
-                $form_data = array(
-                    'name' => Input::get('applicant-name'),
-                    'email' => Input::get('applicant-email'),
-                    'location' => array(
-                        'line1' => Input::get('applicant-street'),
-                        'city' => Input::get('applicant-city'),
-                        'zip' => Input::get('applicant-zip'), 
-                        'country' => Input::get('applicant-country'),
-                    ),
-                    'phone' => Input::get('applicant-phone'),
-                    'gender' => Input::get('gender'),
-                    'birthdate' => Input::get('dob'),
-                    'note' => array(
-                        'text' => Input::get('applicant-message')
-                    ),
-                    'sources' => array(
-                        array(
-                            'parent_source_id' => Input::get('applicant-find'),
-                            'name' => 'Applicant'
-                        )
-                    ),
-                    'job_title' => Job::where('job_id', Input::get('job-id'))->value('title'),
-                );
+            $form_data = array(
+                'name' => Input::get('applicant-name'),
+                'email' => Input::get('applicant-email'),
+                'location' => array(
+                    'line1' => Input::get('applicant-street'),
+                    'city' => Input::get('applicant-city'),
+                    'zip' => Input::get('applicant-zip'), 
+                    'country' => Input::get('applicant-country'),
+                ),
+                'phone' => Input::get('applicant-phone'),
+                'gender' => Input::get('gender'),
+                'birthdate' => Input::get('dob'),
+                'note' => array(
+                    'text' => Input::get('applicant-message')
+                ),
+                'sources' => array(
+                    array(
+                        'parent_source_id' => Input::get('applicant-find'),
+                        'name' => 'Applicant'
+                    )
+                ),
+                'job_title' => Job::where('job_id', Input::get('job-id'))->value('title'),
+            );
 
-                if(Input::get('job-id') != 188) {
-                    $form_data['job_link'] = Request::url();
-                }
+            if(Input::get('job-id') != 188) {
+                $form_data['job_link'] = Request::url();
+            }
 
-                if(Input::get('referral-name') && Input::get('referral-email')) {
+            if(Input::get('referral-name') && Input::get('referral-email')) {
 
-                    $form_data['referral_name'] = Input::get('referral-name');
-                    $form_data['referral_email'] = Input::get('referral-email');
-                    
-                    if(Lang::getLocale() == 'en') {
-                        $this->sendMailR($form_data, 'You have referred your friend!', 'referral_en', $form_data['referral_email'], $form_data['referral_name']);
-                    } else {
-                        $this->sendMailR($form_data, 'Je hebt naar je vriend verwezen!', 'referral_nl', $form_data['referral_email'], $form_data['referral_name']);
-                    }
-
-                }
-
-                if(Input::get('form_type') == 'application') {
-                    
-                    if(Lang::getLocale() == 'en') {
-                        $this->sendMail($form_data, 'Thanks for applying for a job at Search It Recruitment', 'application_en');
-                        Flash::success('Thanks for applying for a job at Search It Recruitment');
-                    } else {
-                        $this->sendMail($form_data, 'Bedankt voor solliciteren bij Search It Recruitment', 'application_nl');
-                        Flash::success('Bedankt voor solliciteren bij Search It Recruitment');
-                    }
-
+                $form_data['referral_name'] = Input::get('referral-name');
+                $form_data['referral_email'] = Input::get('referral-email');
+                
+                if(Lang::getLocale() == 'en') {
+                    $this->sendMailR($form_data, 'You have referred your friend!', 'referral_en', $form_data['referral_email'], $form_data['referral_name']);
                 } else {
-                    
-                    if(Lang::getLocale() == 'en') {
-                        $this->sendMail($form_data, 'Thanks for uploading your resume at Search It Recruitment', 'resume_en');
-                        Flash::success('Thanks for uploading your resume at Search It Recruitment');
-                    } else {
-                        $this->sendMail($form_data, 'Bedankt voor het uploaden van jouw cv bij Search It Recruitment', 'resume_nl');
-                        Flash::success('Bedankt voor het uploaden van jouw cv bij Search It Recruitment');
-                    }
+                    $this->sendMailR($form_data, 'Je hebt naar je vriend verwezen!', 'referral_nl', $form_data['referral_email'], $form_data['referral_name']);
+                }
 
+            }
+
+            if(Input::get('form_type') == 'application') {
+                
+                if(Lang::getLocale() == 'en') {
+                    $this->sendMail($form_data, 'Thanks for applying for a job at Search It Recruitment', 'application_en');
+                    Flash::success('Thanks for applying for a job at Search It Recruitment');
+                } else {
+                    $this->sendMail($form_data, 'Bedankt voor solliciteren bij Search It Recruitment', 'application_nl');
+                    Flash::success('Bedankt voor solliciteren bij Search It Recruitment');
                 }
 
             } else {
-
-                if(Input::get('form_type') == 'application') {
-                    Flash::success('app');
+                
+                if(Lang::getLocale() == 'en') {
+                    $this->sendMail($form_data, 'Thanks for uploading your resume at Search It Recruitment', 'resume_en');
+                    Flash::success('Thanks for uploading your resume at Search It Recruitment');
                 } else {
-                    Flash::success('cv');
+                    $this->sendMail($form_data, 'Bedankt voor het uploaden van jouw cv bij Search It Recruitment', 'resume_nl');
+                    Flash::success('Bedankt voor het uploaden van jouw cv bij Search It Recruitment');
                 }
 
             }
