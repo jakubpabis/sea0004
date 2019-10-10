15540a05e2d5246b407055475b41a48bc19f1520
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2019-10-11 00:33:23 +0200

    somelast

--- prototype/src/js/_socialframework.js
+++ prototype/src/js/_socialframework.js
@@ -1,25 +1,3 @@
-$(window).on('load', function() {
-	
-	// window.fbAsyncInit = function() {
-	// 	FB.init({
-	// 		appId            : '382574281913074',
-	// 		autoLogAppEvents : true,
-	// 		xfbml            : true,
-	// 		version          : 'v3.3'
-	// 	});
-	// };
-	
-	// (function(d, s, id){
-	// 	var js, fjs = d.getElementsByTagName(s)[0];
-	// 	if (d.getElementById(id)) {return;}
-	// 	js = d.createElement(s); js.id = id;
-	// 	js.src = "//connect.facebook.net/en_US/sdk.js";
-	// 	fjs.parentNode.insertBefore(js, fjs);
-	// }(document, 'script', 'facebook-jssdk'));
-
-
-});
-
 function isScriptLoaded(url) {
     var scripts = document.getElementsByTagName('script');
     for (var i = scripts.length; i--;) {
